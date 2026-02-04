05c694d3498d8ddeac5e1032339efb37ab9efa53
coderV1 <pawarvikas534@gmail.com>
2026-02-04 15:36:06 +0530

    updated routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
-  createProject,
-  getProjects,
-  getProjectById,
-  updateProject,
-  deleteProject,
-  addTeamMember,
-  removeTeamMember,
+    addTeamMember,
+    archiveProject,
+    createProject,
+    deleteProject,
+    getProjectById,
+    getProjects,
+    removeTeamMember,
+    updateMemberRole,
+    updateProject,
 } from '../controllers/projectController.js';
 import { authMiddleware } from '../middleware/auth.js';
 
@@ -19,7 +21,11 @@ router.get('/', getProjects);
 router.get('/:id', getProjectById);
 router.put('/:id', updateProject);
 router.delete('/:id', deleteProject);
+router.patch('/:id/archive', archiveProject);
 router.post('/:id/members', addTeamMember);
+router.post('/:id/team', authMiddleware, addTeamMember);
+router.delete('/:id/team', authMiddleware, removeTeamMember);
 router.delete('/:id/members', removeTeamMember);
+router.patch('/:id/members/role', updateMemberRole);
 
 export default router;
